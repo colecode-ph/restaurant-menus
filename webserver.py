63e20b06e2d9f49237ba9ac89951a2068dd95be6
Tom Cole <colecode.ph@gmail.com>
2017-07-23 23:05:09 +0800

    Add ability to create a restaurant in the database

--- webserver.py
+++ webserver.py
@@ -28,7 +28,8 @@ class webserverHandler(BaseHTTPRequestHandler): # extends BaseHTTPRequestHandler
 
                 output = ""  # include some content to send back to the client
                 output += "<html><body>"
-                output += "<h1>Testing Web Server - if you can read this it's working!</h1>"
+                output += "<h1>The List of Restaurants</h1>"
+                output += "<h2><a href='/restaurants/new'>Make a New Restaurant</a></h2>"
                 output += "<ul>"
                 names = []
                 restaurantNames = session.query(Restaurant.name).order_by(Restaurant.name)
@@ -39,51 +40,57 @@ class webserverHandler(BaseHTTPRequestHandler): # extends BaseHTTPRequestHandler
                         .format(restaurantName[0])
                         )
                 output += "<ul>"
-
                 output += "</body></html>"
-
                 self.wfile.write(output) # function sends message back to client
-                print (output) # see the output string in the terminal for debugging
+                print ("HTML rendered to client") # see the output string in the terminal for debugging
                 return  # exit the if statement
 
+            if self.path.endswith("/restaurants/new"): # pattern matching that looks for end of URL
+                self.send_response(200)  # send response indicating successful GET request
+                self.send_header('Content-type', 'text/html') # replying with HTML format
+                self.end_headers() # sends a blank line indicating the end of HTTP headers
+
+                output = ""  # include some content to send back to the client
+                output += "<html><body>"
+                output += "<h1>Make a New Restaurant</h1>"
+                output += "<form method='POST' enctype='multipart/form-data'"
+                output += "action='/restaurants/new'>"
+                output += "<input type='text' name='newrestaurantName'>"
+                output += "<button type='submit'>Create</button></form>"
+                output += "</body></html>"
+                self.wfile.write(output) # function sends message back to client
+                print ("HTML rendered to client") # see the output string in the terminal for debugging
+                return  # exit the if statement
 
         except IOError:
             self.send.error(404, "File not found %s" % self.path) # Notify of error
 
     def do_POST(self):
         try:
-            self.send_response(301)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            print "hello!"
-            # parse_header function parses an HTML form header, such as 'content-type'
-            # into a main value, and a dictionary of parameters
-            # uses parse_multipart to collect all the fields in the form
-            # check if form data is being recieved
-            # make variable to get out the value of a specific field or set of
-            # fields and store them in an array
+            if self.path.endswith("/restaurants/new"):
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    messagecontent = fields.get('newrestaurantName')
+                else:
+                    print ("FAIL!")
 
-            ctype, pdict = cgi.parse_header(
-                self.headers.getheader('content-type'))
-            if ctype == 'multipart/form-data':
-                fields = cgi.parse_multipart(self.rfile, pdict)
-                messagecontent = fields.get('message')
+                # create a new Restaurant class
+                newRestaurant = Restaurant(name = messagecontent[0])
+                session.add(newRestaurant)
+                session.commit()
 
-            output = ""
-            output += "<html><body>"
-            output += "<h2>Okay, how about this: </h2>"
-            output += "<h1> %s </h1>" % messagecontent[0]
-            output += "<h1>Process over Product - Lasting Habits Change lives!</h1>"
-            # return the first value of the array that was created via form submission
-            output += (
-                "<form method='POST' enctype='multipart/form-data' action='/hello'><h2>"
-                "What would you like me to say?</h2><input name='message' type='text'><input"
-                " type='submit' value='Submit'></form>")
-            # write input field as 'message' to coincide with the message field in the POST
-            output += "</body></html>"
-            self.wfile.write(output) # function sends message back to client
-            print (output) # see the output string in the terminal for debugging
-
+                self.send_response(301)
+                self.send_header('Content-type', 'text/html')
+                self.send_header('Location', '/restaurants')
+                self.end_headers()
+                print "hello!"
+                # parse_header function parses an HTML form header, such as 'content-type'
+                # into a main value, and a dictionary of parameters
+                # uses parse_multipart to collect all the fields in the form
+                # check if form data is being recieved
+                # make variable to get out the value of a specific field or set of
+                # fields and store them in an array
         except:
             pass
 
